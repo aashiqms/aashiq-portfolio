07b21e9da8717dc920a04063f3f82eab5cff4cba
Aashiq M S <aashiqms@outlook.com>
2023-07-11 22:09:53 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'aashiqms-github-io';
+  title = 'aashiqms';
+
+  currentHeader: number = 0;
 
   downloadResume() {
     window.open('/assets/files/resume_aashiq_june.pdf', '_blank');
